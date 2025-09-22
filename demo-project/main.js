f8a203067bb0b38723fbfec874336b5d0f9f9ef5
bwatkins0428 <bwatkins0428@gmail.com>
2025-09-22 14:26:07 +0000

    demo 1 header

--- demo-project/main.js
+++ demo-project/main.js
@@ -1,22 +1,6 @@
-$(document).ready(function () {
-
+$(document).ready(() => {
     const mobileNav = $('#mobile-nav');
     const headerBottomNav = $('#header-bottom-nav');
 
-    if($(window).width() >= 992) {
-        headerBottomNav.css({'display': 'block'});
-    } else {
-        headerBottomNav.css({'display': 'none'});
-    }
-
     mobileNav.click(() => headerBottomNav.toggle());
-
-
-    $(window).resize(function() {
-        if($(window).width() >= 992) {
-            headerBottomNav.css({'display': 'block'});
-        } else {
-            headerBottomNav.css({'display': 'none'});
-        }
-    });
 });
